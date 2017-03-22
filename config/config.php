f1b0f283ed7e9c0108ddcb275828590139628364
Rhys Bowles <rhys@digitdev.co.uk>
2017-03-22 00:01:00 +0000

    Removed details

--- config/config.php
+++ config/config.php
@@ -2,11 +2,11 @@
 
 //Database Settings
 define( 'DB_HOST', 'localhost' );
-define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_USER', 'username' );
+define( 'DB_PASSWORD', 'password' );
 define( 'DB_NAME', 'seocheck' );
 define( 'PREFIX', 'sc_' );
 
 // Email settings
-define( 'WEBMASTER_EMAIL', 'rhys.wf.bowles@gmail.com' );
+define( 'WEBMASTER_EMAIL', 'your@email.here' );
 define( 'HTML_EMAIL', true );
