026071849a07ed025149669e8d4e7cd535d970e2
Tim Novis <tim.novis@me.com>
2017-03-22 18:59:25 +0000

    redacted email

--- config/config.php
+++ config/config.php
@@ -2,11 +2,11 @@
 
 //Database Settings
 define( 'DB_HOST', 'localhost' );
-define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_USER', 'username' );
+define( 'DB_PASSWORD', 'password' );
 define( 'DB_NAME', 'seocheck' );
 define( 'PREFIX', 'sc_' );
 
 // Email settings
-define( 'NOTIFY_EMAIL', 'tim@novis.co' );
+define( 'NOTIFY_EMAIL', 'your@email.here' );
 define( 'HTML_EMAIL', true );
